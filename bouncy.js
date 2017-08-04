1b2c9e5018a54c12f8a73e0efbc8e5fb887809cf
kalana1985 <kalana1on1@gmail.com>
2017-08-03 22:20:24 -0500

    creating a bouncy circle

--- bouncy.js
+++ bouncy.js
@@ -7,3 +7,24 @@ canvas.height=window.innerHeight;
 //will use this const to drow the items to the canvas
 
 const c = canvas.getContext('2d');
+
+
+//creating a circle
+
+
+
+
+//create a function to make circle move
+//by putting animate inside the requestAni..function 
+//we create a loop with in the function 
+var x = Math.random()*innerWidth;
+var y = Math.random() * innerHeight;
+function animate(){
+	 requestAnimationFrame(animate);
+	 c.beginPath();
+     c.arc(x,y,30,0,Math.PI * 2,false);
+     c.strokeStyle = "blue";
+     c.stroke();
+	 
+}
+animate();
